c90eb55b1110ac4c9445e82780538c6a260135d4
Artal Druk <artald@wix.com>
2021-11-09 12:38:38 +0200

    Add injectHook test

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 /* eslint-disable max-classes-per-file */
-import React from 'react';
-import { create } from 'react-test-renderer';
+import React, { useState, useEffect } from 'react';
+import { create, act } from 'react-test-renderer';
 import {
   Graph, injectComponent, injectHook, ObjectGraph, Obsidian, Provides,
 } from './index';
@@ -45,5 +45,84 @@ describe('Sanity', () => {
     const testInstance = create(<Wrapped />).root;
     const { pageProvider } = testInstance.findByType(Link).props;
     expect(pageProvider.page).toEqual(mockLink);
+  });
+
+  it('Injects to hook ', () => {
+    const mockFriendId = 'mock_friend_id';
+
+    interface FriendProps {
+      friend: Friend;
+    }
+
+    class Friend {
+      get id() {
+        return mockFriendId;
+      }
+    }
+
+    @Graph()
+    class FriendGraph extends ObjectGraph<FriendProps> {
+      @Provides()
+      get friend(): Friend {
+        return new Friend();
+      }
+    }
+
+    class ChatAPI {
+      static subscribers: Map<string, Function> = new Map();
+      static subscribeToFriendStatus(friendId: string, statusChangeHandler: Function) {
+        ChatAPI.subscribers.set(friendId, statusChangeHandler);
+      }
+
+      static unsubscribeFromFriendStatus(friendId: string, statusChangeHandler: Function) {
+        ChatAPI.subscribers.delete(friendId);
+      }
+
+      static notifyFriendStatus(friendId, status) {
+        const handler = ChatAPI.subscribers.get(friendId);
+        if (handler) {
+          handler({ isOnline: status });
+        }
+      }
+    }
+
+    function useFriendStatus({ friend }) {
+      const [isOnline, setIsOnline] = useState(null);
+
+      function handleStatusChange(status) {
+        setIsOnline(status.isOnline);
+      }
+
+      useEffect(() => {
+        ChatAPI.subscribeToFriendStatus(friend.id, handleStatusChange);
+        return () => {
+          ChatAPI.unsubscribeFromFriendStatus(friend.id, handleStatusChange);
+        };
+      });
+
+      return { isOnline, friendId: friend.id };
+    }
+
+    const injectedUseFriendStatus = injectHook(useFriendStatus, FriendGraph);
+
+    function FriendStatus() {
+      const { isOnline, friendId } = injectedUseFriendStatus();
+
+      if (isOnline === null) {
+        return 'Loading...';
+      }
+      return isOnline ? `${friendId} Online` : `${friendId} Offline`;
+    }
+
+    let testRenderer;
+    act(() => {
+      testRenderer = create(<FriendStatus />);
+    });
+
+    ChatAPI.notifyFriendStatus(mockFriendId, true);
+    expect(testRenderer.root.findByType(FriendStatus).children[0]).toEqual('mock_friend_id Online');
+
+    ChatAPI.notifyFriendStatus(mockFriendId, false);
+    expect(testRenderer.root.findByType(FriendStatus).children[0]).toEqual('mock_friend_id Offline');
   });
 });
