a5dd0b66531c5f919178c02ae4db759025c8342a
Artal Druk <artald@wix.com>
2021-11-10 14:50:20 +0200

    Add example project

---
+++ ReactNativeExample/App.tsx
@@ -0,0 +1,47 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {ReactNode} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+import MyInjectedComponent from './MyInjectedComponent';
+
+const App: () => ReactNode = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <MyInjectedComponent />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ jest.config.js
@@ -0,0 +1,10 @@
+const config = {
+  resetMocks: true,
+  resetModules: true,
+  roots: [
+    'node_modules',
+    'src',
+  ],
+};
+
+module.exports = config;
