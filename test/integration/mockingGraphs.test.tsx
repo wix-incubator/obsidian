c575b0835ebe9ccf030db0727882e53b77b782e4
Artal Druk <artald@wix.com>
2021-12-02 16:35:25 +0200

    Initial TestKit for mocking graphs

--- test/integration/mockingGraphs.test.tsx
+++ test/integration/mockingGraphs.test.tsx
@@ -1,38 +1,21 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { Constructable } from '@Obsidian';
-import { GraphMiddleware, ResolveChain } from '../../src/graph/registry/GraphMiddleware';
 import MainGraph from './fixtures/MainGraph';
 import Subgraph from './fixtures/Subgraph';
-import { Graph, Obsidian, Provides } from '../../src';
+import { Graph, Provides } from '../../src';
 import InjectedComponent from './fixtures/InjectedComponent';
-import ObjectGraph from '../../src/graph/Graph';
+import testKit from '../../testkit';
 
 describe('Test doubles', () => {
   let Component: React.FunctionComponent;
-  const graphMiddleware = new class extends GraphMiddleware {
-    resolve<T extends ObjectGraph, Props>(resolveChain: ResolveChain, Graph: Constructable<T>, props?: Props) {
-      switch (Graph.name) {
-        case MainGraph.name:
-          return new MockMainGraph(props) as unknown as T;
-        case Subgraph.name:
-          return new MockSubgraph(props) as unknown as T;
-        default:
-          return resolveChain.proceed(Graph, props);
-      }
-    }
-  }();
-
-  beforeAll(() => {
-    Obsidian.addGraphMiddleware(graphMiddleware);
-  });
 
   beforeEach(() => {
     Component = InjectedComponent;
-  });
 
-  afterAll(() => {
-    Obsidian.clearGraphMiddlewares();
+    testKit.mockGraphs({
+      MainGraph: MockMainGraph,
+      Subgraph: MockSubgraph,
+    });
   });
 
   it('Supports replacing graphs with test doubles', () => {
