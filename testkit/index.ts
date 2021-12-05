02d26e888d6d7e7e82fdbbccc3eb2e68ad31ce5d
Artal Druk <artald@wix.com>
2021-12-05 09:40:38 +0200

    Initial TestKit for mocking graphs (#30)

---
+++ testkit/index.ts
@@ -0,0 +1,25 @@
+import { GraphMiddleware, ResolveChain } from '../src/graph/registry/GraphMiddleware';
+import ObjectGraph from '../src/graph/Graph';
+import { Constructable } from '../src/@types/obsidian';
+import { Obsidian } from '../src';
+
+beforeEach(() => {
+  Obsidian.clearGraphMiddlewares();
+});
+
+class Index {
+  mockGraphs(graphNameToGraph: Record<string, Constructable<ObjectGraph>>) {
+    const graphMiddleware = new class extends GraphMiddleware {
+      resolve<T extends ObjectGraph, Props>(resolveChain: ResolveChain, Graph: Constructable<T>, props?: Props) {
+        if (graphNameToGraph[Graph.name]) {
+          const TheGraph = graphNameToGraph[Graph.name];
+          return new TheGraph(props) as unknown as T;
+        }
+        return resolveChain.proceed(Graph, props);
+      }
+    }();
+    Obsidian.addGraphMiddleware(graphMiddleware);
+  }
+}
+
+export default new Index();
