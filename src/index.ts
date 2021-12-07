2bbfd94c668f7fb5fdb18fec516b890b6f278e09
Guy Carmeli <guyc@wix.com>
2021-12-07 21:36:29 +0200

    Slightly refactor hooks injection + maybe fix local env after integrating BuildKite

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,14 @@ import React from 'react';
 import { Constructable } from '@Obsidian';
 import _Graph from './decorators/Graph';
 import _ObjectGraph from './graph/ObjectGraph';
-import _injectHook from './injectHook';
 import ComponentInjector from './injectors/components/ComponentInjector';
 import _Provides from './Provides';
 import { Inject as _Inject, Injectable as _Injectable } from './decorators/injectClass';
 import _obsidian from './Obsidian';
+import HookInjector from './injectors/hooks/HookInjector';
 
 export const Graph = _Graph;
 export const ObjectGraph = _ObjectGraph;
-export const injectHook = _injectHook;
 export const Provides = _Provides;
 export const Injectable = _Injectable;
 export const Inject = _Inject;
@@ -21,3 +20,9 @@ export const injectComponent = <P> (
   __Target: React.FunctionComponent<P>,
   __Graph: Constructable<_ObjectGraph>,
 ) => componentInjector.inject(__Target, __Graph);
+
+const hookInjector = new HookInjector();
+export const injectHook = <Args, Result> (
+  hook: (args: Args) => Result,
+  __Graph: Constructable<_ObjectGraph>,
+) => hookInjector.inject(hook, __Graph);
--- src/injectors/components/useGraph.ts
+++ src/injectors/components/useGraph.ts
@@ -4,7 +4,7 @@ import ObjectGraph from 'src/graph/ObjectGraph';
 import graphRegistry from '../../graph/registry/GraphRegistry';
 import referenceCounter from '../../ReferenceCounter';
 
-export default <P>(Graph: Constructable<ObjectGraph>, props: Partial<P>) => {
+export default <P>(Graph: Constructable<ObjectGraph>, props?: Partial<P>) => {
   const [graph] = useState(graphRegistry.resolve(Graph, props));
   useEffect(() => {
     referenceCounter.retain(graph);
