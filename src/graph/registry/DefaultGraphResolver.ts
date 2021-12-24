fde81af89b8da9850d690cabde6818e689ca3b99
Guy Carmeli <guyc@wix.com>
2021-12-24 14:49:06 +0200

    fix compilation errors

--- src/graph/registry/DefaultGraphResolver.ts
+++ src/graph/registry/DefaultGraphResolver.ts
@@ -1,9 +1,10 @@
 import { Constructable } from '../../types';
 import { Graph } from '../Graph';
-import { Middleware, ResolveChain } from './Middleware';
+import { Middleware } from './Middleware';
+import { ResolveChain } from './ResolveChain';
 
 export default class DefaultGraphMiddleware<T extends Graph> extends Middleware<T> {
-  resolve<Props>(_resolveChain: ResolveChain<T>, Graph: Constructable<T>, props?: Props): T {
+  resolve(_resolveChain: ResolveChain<T>, Graph: Constructable<T>, props?: any): T {
     return new Graph(props);
   }
 }
--- testkit/index.ts
+++ testkit/index.ts
@@ -1,9 +1,9 @@
+import { GraphResolveChain } from 'src/graph/registry/GraphResolveChain';
 import {
   ObjectGraph,
   Constructable,
   GraphMiddleware,
   Obsidian,
-  ResolveChain,
 } from '../src';
 
 beforeEach(() => {
@@ -13,7 +13,7 @@ beforeEach(() => {
 class Index {
   mockGraphs(graphNameToGraph: Record<string, Constructable<ObjectGraph>>) {
     const graphMiddleware = new class extends GraphMiddleware {
-      resolve<Props>(resolveChain: ResolveChain<ObjectGraph>, Graph: Constructable<ObjectGraph>, props?: Props) {
+      resolve<Props>(resolveChain: GraphResolveChain, Graph: Constructable<ObjectGraph>, props?: Props) {
         if (graphNameToGraph[Graph.name]) {
           const TheGraph = graphNameToGraph[Graph.name];
           return new TheGraph(props);
