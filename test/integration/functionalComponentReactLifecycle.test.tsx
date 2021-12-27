87b3dca65a661aa5db438977093ecce218204b37
Guy Carmeli <guyc@wix.com>
2021-12-27 08:00:34 +0200

    No need to import reflect-metadata here

--- test/integration/functionalComponentReactLifecycle.test.tsx
+++ test/integration/functionalComponentReactLifecycle.test.tsx
@@ -1,4 +1,3 @@
-import 'reflect-metadata';
 import React, { useCallback, useEffect, useState } from 'react';
 import { fireEvent, render } from '@testing-library/react';
 import { injectComponent } from '../../src';
