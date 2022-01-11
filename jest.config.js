a269cafc703f4151d00b54cf40b0481ad4c994af
Artal Druk <artald@wix.com>
2022-01-11 16:04:21 +0200

    - revert usage of ts-jest - make tests pass after upgrade to Jest 27: this required to set the env to jsdom which required polyfill for setImmediate which is used by ts-mockito

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,4 @@
-// const {defaults: tsjPreset} = require('ts-jest/presets');
 const config = {
-  preset: 'ts-jest',
   resetMocks: true,
   resetModules: true,
   roots: [
@@ -9,29 +7,10 @@ const config = {
     'transformers',
     'test',
   ],
-  testPathIgnorePatterns: [
-    "/node_modules/"
-  ],
-  globals: {
-    'ts-jest': {
-      babelConfig: {
-        plugins: [
-          `${__dirname}/dist/transformers/babel-plugin-obsidian-provide`,
-          ['@babel/plugin-proposal-decorators', { legacy: true }],
-          ['@babel/plugin-proposal-class-properties', { legacy: true }],
-          'babel-plugin-parameter-decorator'
-        ],
-        presets: [
-          ['@babel/preset-env', { targets: { node: 'current' } }],
-          ['@babel/preset-typescript', {'onlyRemoveTypeImports': true}],
-        ]
-      }
-    }
-  },
   setupFilesAfterEnv: [
-    "./test/jest.setup.ts"
+    '<rootDir>/node_modules/setimmediate/setimmediate.js'
   ],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  testEnvironment: 'jsdom'
 };
 
 module.exports = config;
