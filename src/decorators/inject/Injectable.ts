148a3d8c595604bac59586b63148558c0df3d16f
Artal Druk <artald@wix.com>
2022-01-12 10:53:38 +0200

    fix imports

--- src/decorators/inject/Injectable.ts
+++ src/decorators/inject/Injectable.ts
@@ -1,7 +1,7 @@
-import { Constructable } from '../types';
-import { Graph } from '../graph/Graph';
-import graphRegistry from '../graph/registry/GraphRegistry';
-import ClassInjector from '../injectors/class/ClassInjector';
+import { Constructable } from '../../types';
+import { Graph } from '../../graph/Graph';
+import graphRegistry from '../../graph/registry/GraphRegistry';
+import ClassInjector from '../../injectors/class/ClassInjector';
 
 export function Injectable(Graph: Constructable<Graph>): any {
   return new ClassInjector(graphRegistry).inject(Graph);
