651a09aabd69ad663ce3b811abf1826c5877046f
Artal Druk <artald@wix.com>
2022-01-12 16:39:16 +0200

    Fix the default render test

--- example/__tests__/App-test.js
+++ example/__tests__/App-test.js
@@ -4,7 +4,7 @@
 
 import 'react-native';
 import React from 'react';
-import Explore from '../screens/App';
+import Explore from '../screens/Explore';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
