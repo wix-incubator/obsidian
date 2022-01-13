179451e426d03ce13576b345aa4c069b215f87bb
Guy Carmeli <guyc@wix.com>
2022-01-13 18:35:37 +0200

    injects simple constructor args

--- test/integration/classInjection.test.tsx
+++ test/integration/classInjection.test.tsx
@@ -20,6 +20,16 @@ describe('Class injection', () => {
     ) { }
   }
 
+  @Injectable(MainGraph)
+  class SimpleArgsClass {
+    readonly someString: string;
+
+    constructor(anotherString?: string, someString?: string);
+    public constructor(@Inject() someString: string, @Inject() public anotherString: string) {
+      this.someString = someString;
+    }
+  }
+
   it('injects class properties', () => {
     const uut = new SingleArgClass();
     expect(uut.someString).toBe(injectedValues.fromStringProvider);
@@ -39,6 +49,12 @@ describe('Class injection', () => {
   it('only injects if constructor arg is undefined', () => {
     const uut = new MultiArgClass('override');
     expect(uut.someString).toBe('override');
+    expect(uut.anotherString).toBe(injectedValues.anotherString);
+  });
+
+  it('injects simple constructor args', () => {
+    const uut = new SimpleArgsClass();
+    expect(uut.someString).toBe(injectedValues.fromStringProvider);
     expect(uut.anotherString).toBe(injectedValues.anotherString);
   });
 });
