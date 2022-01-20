dc0a670e4f310dd1c5d6c52f1f244e596c4941e3
Eli Gutovsky <eligu@wix.com>
2022-01-20 13:52:37 +0200

    add name parameter to lazy inject

--- src/decorators/inject/LazyInject.ts
+++ src/decorators/inject/LazyInject.ts
@@ -1,7 +1,8 @@
 import InjectionMetadata from '../../injectors/class/InjectionMetadata';
 
+export function LazyInject(name?: string) {
   return (target: object, propertyKey: string) => {
     const metadata = new InjectionMetadata();
-    metadata.saveLazyPropertyMetadata(target.constructor, propertyKey);
+    metadata.saveLazyPropertyMetadata(target.constructor, name ?? propertyKey);
   };
 }
