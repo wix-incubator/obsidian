ce8d912de5b231965fc397b5b995f30496e0d340
Eli Gutovsky <eligu@wix.com>
2022-01-20 15:32:25 +0200

    chanage Object to object

--- src/decorators/inject/LazyInject.ts
+++ src/decorators/inject/LazyInject.ts
@@ -1,7 +1,7 @@
 import InjectionMetadata from '../../injectors/class/InjectionMetadata';
 
 export function LazyInject(name?: string): any {
-  return (target: Object) => {
+  return (target: object) => {
     const metadata = new InjectionMetadata();
     metadata.saveLazyPropertyMetadata(target.constructor, name!);
   };
