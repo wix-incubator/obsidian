51dc40776a3cb649a15ae7e8efe0e8a71d01ebaa
Guy Carmeli <guyc@wix.com>
2022-02-19 15:54:05 +0200

    Maybe fix tests

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }],
+    ['@babel/preset-env', { targets: { node: 'current' }, loose: true }],
     ['@babel/preset-typescript', {'onlyRemoveTypeImports': true}],
     '@babel/preset-react',
   ],
