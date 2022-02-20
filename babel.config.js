65706f0d7a87261a1c7f8109903742da233b8f7b
Guy Carmeli <guyc@wix.com>
2022-02-20 19:28:19 +0200

    Reenable constructor param injection tests

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env', { targets: { node: 'current' }, loose: true }],
+    ['@babel/preset-env', { targets: { node: 'current' }}],
     ['@babel/preset-typescript', {'onlyRemoveTypeImports': true}],
     '@babel/preset-react',
   ],
   plugins: [
     `${__dirname}/dist/transformers/babel-plugin-obsidian`,
     ['@babel/plugin-proposal-decorators', { legacy: true }],
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    '@babel/plugin-proposal-class-properties',
     'babel-plugin-parameter-decorator'
   ],
 };
