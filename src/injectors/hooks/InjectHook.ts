e0489c7f213e84f2e30b92080b8a810640811f11
Guy Carmeli <guyc@wix.com>
2022-06-29 19:58:21 +0300

    Commit missing changes

--- src/injectors/hooks/InjectHook.ts
+++ src/injectors/hooks/InjectHook.ts
@@ -7,12 +7,14 @@ interface Descriminator {
 }
 
 const hookInjector = new HookInjector();
-export const injectHook = <OwnProps = Descriminator, InjectedProps = Descriminator, Result = unknown> (
+export const injectHook = <OwnProps = Descriminator, InjectedProps = Descriminator, Result = Descriminator> (
   hook: (props: (
     OwnProps extends infer P ? OwnProps extends Descriminator ? P : OwnProps : never) &
   (InjectedProps extends Descriminator ? any : InjectedProps
-  )) => Result,
+  )) => Result extends infer R ? Result extends Descriminator ? R : Result : never,
   Graph: Constructable<ObjectGraph>,
 ) => hookInjector.inject(hook, Graph) as (props: InjectedProps extends Descriminator ?
     OwnProps extends Descriminator ? Partial<OwnProps> : OwnProps :
-    OwnProps extends InjectedProps ? Partial<OwnProps> : OwnProps & Partial<InjectedProps>) => Result;
+    OwnProps extends InjectedProps ? Partial<OwnProps> : OwnProps & Partial<InjectedProps>) => (
+    Result extends infer R ? Result extends Descriminator ? R : Result : never
+  );
