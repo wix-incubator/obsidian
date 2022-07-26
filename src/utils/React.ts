dfffbeff9e0a115178728a5d105f67c90cfed2fc
Serhii Tuniehov <serhiit@wix.com>
2022-07-26 16:04:13 +0300

    fixed memo issue

--- src/utils/React.ts
+++ src/utils/React.ts
@@ -1,8 +1,15 @@
-import { FunctionComponent } from 'react';
+import React, { FunctionComponent } from 'react';
 
 type MemoizedComponent = React.MemoExoticComponent<FunctionComponent<any>> & {
   compare?: (prevProps: any, nextProps: any) => boolean;
 };
 export function isMemoizedComponent(component: FunctionComponent<any>): component is MemoizedComponent {
   return (component as MemoizedComponent).type !== undefined;
+}
+
+export function genericMemo<C extends React.ComponentType<any>>(
+  Component: Parameters<typeof React.memo>[0],
+  propsAreEqual?: Parameters<typeof React.memo>[1],
+) {
+  return (React.memo(Component, propsAreEqual) as unknown) as C;
 }
