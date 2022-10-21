9ca93e6108b072da6d27e7b26d288bd6d79ffb61
Guy Carmeli <guyc@wix.com>
2022-10-21 21:44:08 +0300

    Change terminology from Scope to LifecycleBound

---
+++ test/integration/lifecyleBoundGraphs.test.tsx
@@ -0,0 +1,35 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import { injectComponent, injectHook } from '../../src';
+import { LifecycleBoundGraph } from '../fixtures/LifecycleBoundGraph';
+
+describe('React lifecycle bound graphs', () => {
+  const Component = createComponent();
+
+  beforeEach(() => {
+    LifecycleBoundGraph.timesCreated = 0;
+  });
+
+  it('creates a bound graph only once', async () => {
+    render(<Component />);
+    render(<Component />);
+    expect(LifecycleBoundGraph.timesCreated).toBe(1);
+  });
+
+  it('clears a bound graph when all dependent components and hooks are unmounted', () => {
+    const { unmount } = render(<Component />);
+    unmount();
+    render(<Component />);
+
+    expect(LifecycleBoundGraph.timesCreated).toBe(2);
+  });
+
+  function createComponent() {
+    const useHook = injectHook(() => {}, LifecycleBoundGraph);
+
+    return injectComponent(() => {
+      useHook();
+      return <></>;
+    }, LifecycleBoundGraph);
+  }
+});
