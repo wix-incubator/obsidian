68fc99bf7cdccafab9055338dbd5cd23b89d3d6f
Guy Carmeli <guyc@wix.com>
2023-06-24 05:55:43 +0300

    Support React 18 strict mode
    React 18 changes how strict mode works in DEV - When a component is mounted, React now unmounts it then immediately mounts it again with the previous state.
    This surfaced a crash that mostly happened during hot refresh.

---
+++ test/integration/reactStrictMode.test.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { DependenciesOf, injectComponent } from '../../src';
+import { LifecycleBoundGraph } from '../fixtures/LifecycleBoundGraph';
+
+describe('React Strict Mode', () => {
+  it('should render without crashing', () => {
+    const { container } = render(<InjectedComponent stringFromProps={'foo'}/>, {
+      wrapper: React.StrictMode,
+    });
+    expect(container.textContent).toBe('A string passed via props: foo');
+  });
+});
+
+const Component = ({ computedFromProps }: DependenciesOf<LifecycleBoundGraph, 'computedFromProps'>) => {
+  return <>{computedFromProps}</>;
+};
+
+const InjectedComponent = injectComponent<{ stringFromProps: string }>(Component, LifecycleBoundGraph);
