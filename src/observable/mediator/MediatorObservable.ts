803e7551971fd54cbb6b9c2fda006a8a96d2b96a
Guy Carmeli <>
2023-08-05 12:05:15 +0300

    Implement MediatorObservable.mapSource

--- src/observable/mediator/MediatorObservable.ts
+++ src/observable/mediator/MediatorObservable.ts
@@ -1,7 +1,15 @@
 import { Observable } from '../Observable';
-import { OnNext } from '../types';
+import { Mapper, OnNext } from '../types';
 
 export class MediatorObservable<T> extends Observable<T> {
+  mapSource<Source, Result extends T>(source: Observable<Source>, mapNext: Mapper<Source, Result>) {
+    this.addSource<Source>(source, (next) => {
+      const mapped = mapNext(next, this.value as Result) as Result;
+      this.value = mapped;
+    });
+    return this;
+  }
+
   addSource<S>(source: Observable<S>, onNext: OnNext<S>) {
     source.subscribe(onNext);
     if (source.value !== undefined) {
