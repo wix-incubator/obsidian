5a03a6ff02b7e2a466e8149de68c1f04430670d3
Guy Carmeli <>
2023-08-28 19:43:13 -0400

    Add ability to map multiple observables in a single callback
    A common case with MediatorObservables is the need to create a side effect based on multiple Observables. The mapSource function maps a single source. When it's used to map multiple sources, in order to adhere to the DRY principle we won't be able to inline to onNext callback as it needs to be shared in multiple calls to mapSource.
    
    This PR adds the mapSources function which receives multiple sources and consequently makes it easier to share the same callback.

--- src/observable/types.ts
+++ src/observable/types.ts
@@ -1,4 +1,5 @@
 export type OnNext<T> = (value: T) => void | undefined; // OnNext callbacks should never return a value
+
 export type Mapper<Other, Mine> = (next: Other, currentValue: Mine) => Mine extends void ?
   'A map function must return a value. Check your map function and ensure it has a valid return statement.' :
   Mine;
@@ -10,3 +11,20 @@ export interface Observable<T> {
 }
 
 export type ObservedValues<T> = { [K in keyof T]: T[K] extends Observable<infer R> ? R : never };
+
+export type MultiMapper<Mine, S1, S2, S3, S4, S5> = (
+  [S1, S2, S3, S4, S5]: Args<S1, S2, S3, S4, S5>,
+  currentValue: Mine,
+) => Mine;
+
+export type Observables<S1, S2, S3, S4, S5> =
+  [Observable<S1>, Observable<S2>] |
+  [Observable<S1>, Observable<S2>, Observable<S3>] |
+  [Observable<S1>, Observable<S2>, Observable<S3>, Observable<S4>] |
+  [Observable<S1>, Observable<S2>, Observable<S3>, Observable<S4>, Observable<S5>];
+
+export type Args<A1, A2, A3, A4, A5> =
+  [A1, A2] |
+  [A1, A2, A3] |
+  [A1, A2, A3, A4] |
+  [A1, A2, A3, A4, A5];
