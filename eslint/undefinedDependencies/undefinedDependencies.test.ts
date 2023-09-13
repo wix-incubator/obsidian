87230e64bf9fa48c19f07104670b42ae630e3144
orlyd <orlyd@wix.com>
2023-09-13 09:05:48 +0300

    -

--- eslint/undefinedDependencies/undefinedDependencies.test.ts
+++ eslint/undefinedDependencies/undefinedDependencies.test.ts
@@ -2,20 +2,22 @@ import { RuleTester } from '@typescript-eslint/rule-tester';
 import { undefinedDependencies } from './ruleConfiguration';
 import { validGraph } from './testUtils/validGraphs';
 import { invalidGraph } from './testUtils/invalidGraphs';
+import { create } from './createFunction';
 
-// describe('createFunction', () => {
-//   let uut: (context: any) => any;
+describe('createFunction', () => {
+  let uut: (context: any) => any;
 
-//   beforeEach(() => {
-//     uut = create;
-//   });
+  beforeEach(() => {
+    uut = create;
+  });
 
-//   it('defines a create function', () => {
-//     expect(uut).toBeDefined();
-//   });
-// });
+  it('defines a create function', () => {
+    expect(uut).toBeDefined();
+  });
+});
 
 const ruleTester = new RuleTester();
+// simple graph test
 ruleTester.run('undefined-dependency', undefinedDependencies, {
   valid: [validGraph],
   invalid: [{
@@ -25,3 +27,5 @@ ruleTester.run('undefined-dependency', undefinedDependencies, {
     }],
   }],
 });
+
+// graph with subgraph test
