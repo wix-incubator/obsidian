91fc15b7f6b2e3c27413276e52d0e1852ea86105
Guy Carmeli <>
2024-01-08 11:35:46 +0200

    Document mockModel

--- testkit/mockModel.ts
+++ testkit/mockModel.ts
@@ -1,6 +1,13 @@
+import { Constructable } from '../src/types';
 import { Model } from '../src/model/Model';
 
-export function mockModel<T extends Model, S extends Partial<T>>(mock: S): T {
+export function mockModel<T extends Model, S extends Partial<T>>(mock: S, BaseClass?: Constructable<T>): T {
+  if (BaseClass) {
+    const bc = new BaseClass();
+    Object.assign(bc, mock);
+    return bc;
+  }
+
   return new class extends Model {
     constructor() {
       super();
