6239878b2767688a03448f22f2a38efb8161a238
orlyd <orlyd@wix.com>
2024-02-22 14:31:19 +0200

    change to yarn

--- eslint/undefinedDependencies/undefinedDependencies.test.ts
+++ eslint/undefinedDependencies/undefinedDependencies.test.ts
@@ -3,18 +3,6 @@ import { validGraphSimple } from './testUtils/validGraphs';
 import { invalidGraph } from './invalidGraphs';
 import { undefinedDependencies } from './ruleConfiguration';
 
-// describe('createFunction', () => {
-//   let uut: (context: any) => any;
-
-//   beforeEach(() => {
-//     uut = create;
-//   });
-
-//   it('defines a create function', () => {
-//     expect(uut).toBeDefined();
-//   });
-// });
-
 const ruleTester = new RuleTester();
 
 ruleTester.run('undefined-dependency', undefinedDependencies, {
@@ -25,12 +13,11 @@ ruleTester.run('undefined-dependency', undefinedDependencies, {
       messageId: 'undefinedDependency',
     }],
   },
-  // ,
   // {
   //   code: invalidGraphWithSubgraph,
   //   errors: [{
   //     messageId: 'undefinedDependency',
   //   }],
-  // }
+  // },
   ],
 });
