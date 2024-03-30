cbb4299a68d77108e42b00697b55d46d66962737
Guy Carmeli <>
2024-03-30 10:04:40 +0300

    f

--- src/injectors/components/InjectComponent.ts
+++ src/injectors/components/InjectComponent.ts
@@ -2,20 +2,20 @@ import { ObjectGraph } from '../../graph/ObjectGraph';
 import { Constructable } from '../../types';
 import ComponentInjector from './ComponentInjector';
 
-interface Descriminator {
-  obsidianDescriminator: never;
+interface Discriminator {
+  obsidianDiscriminator: never;
 }
 
 const componentInjector = new ComponentInjector();
 
-export const injectComponent = <OwnProps = Descriminator, InjectedProps = Descriminator> (
+export const injectComponent = <OwnProps = Discriminator, InjectedProps = Discriminator> (
   Target: React.FunctionComponent<
-  (OwnProps extends infer P ? OwnProps extends Descriminator ? P : OwnProps : never) &
-  (InjectedProps extends Descriminator ? any : InjectedProps)
+  (OwnProps extends infer P ? OwnProps extends Discriminator ? P : OwnProps : never) &
+  (InjectedProps extends Discriminator ? any : InjectedProps)
   >,
   Graph: Constructable<ObjectGraph>,
 ) => componentInjector.inject(Target, Graph) as React.FunctionComponent<
-  InjectedProps extends Descriminator ?
-    OwnProps extends Descriminator ? Partial<OwnProps> : OwnProps :
+  InjectedProps extends Discriminator ?
+    OwnProps extends Discriminator ? Partial<OwnProps> : OwnProps :
     OwnProps extends InjectedProps ? Partial<OwnProps> : OwnProps & Partial<InjectedProps>
   >;
