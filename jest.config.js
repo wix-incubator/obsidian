07b16618fd83e6f80ab1dd9269cccb791ebe24cd
Guy Carmeli <>
2024-04-05 08:50:39 +0300

    Add coverage report workflow

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,4 @@
+/** @type {import('jest').Config} */
 const config = {
   resetMocks: true,
   resetModules: true,
@@ -12,7 +13,14 @@ const config = {
     './jest.setup-after-env.js',
     'jest-extended/all'
   ],
-  testEnvironment: 'jsdom'
+  testEnvironment: 'jsdom',
+  coveragePathIgnorePatterns: ['/node_modules/', '/tests/'],
+  coverageReporters: ["json-summary", "text"],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!dist/**',
+  ],
+  collectCoverage: true,
 };
 
 module.exports = config;
