cbb45c4040ba229841dfc57bbe16c7f425764803
Guy Carmeli <>
2024-04-15 05:57:49 +0300

    Add dependencies to eslint plugin folder

--- packages/react-obsidian/eslint-local-rules.js
+++
@@ -1,5 +0,0 @@
-const {unresolvedDependencies} = require('./dist/eslint/unresolvedDependencies/ruleConfiguration')
-
-module.exports = {
-  '@obsidian/eslint-plugin-unresolved-dependencies': unresolvedDependencies
-};
