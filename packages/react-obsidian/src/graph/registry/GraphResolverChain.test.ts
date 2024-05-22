542af3c653a9107dc17e6371e99699d12b187374
Guy Carmeli <>
2024-05-22 10:56:51 +0300

    Stop using ts-mockito in favor of jest-when and jest-mock-extended

--- packages/react-obsidian/src/graph/registry/GraphResolverChain.test.ts
+++ packages/react-obsidian/src/graph/registry/GraphResolverChain.test.ts
@@ -1,42 +1,39 @@
-import {
-  instance,
-  mock,
-  when,
-  imock,
-  anything,
-  verify,
-} from '@johanblumenberg/ts-mockito';
+import { mock } from 'jest-mock-extended';
+import { when } from 'jest-when';
 import MainGraph from '../../../test/fixtures/MainGraph';
 import { Middleware } from './Middleware';
 import GraphResolversChain from './GraphMiddlewareChain';
 import { ObjectGraph } from '../ObjectGraph';
 import { GraphMiddleware } from './GraphMiddleware';
 
+
 describe('GraphResolverChain', () => {
   let uut: GraphResolversChain<ObjectGraph>;
   let defaultGraphResolver: GraphMiddleware;
-  const mockGraph = mock(MainGraph);
+  const mockGraph = mock<MainGraph>();
 
   beforeEach(() => {
-    defaultGraphResolver = mock(GraphMiddleware);
-    uut = new GraphResolversChain(instance(defaultGraphResolver));
+    defaultGraphResolver = mock();
+    uut = new GraphResolversChain(defaultGraphResolver);
   });
 
   it('Resolves default', () => {
     const props = { a: 'b' };
-    when(defaultGraphResolver.resolve(anything(), MainGraph, props)).thenReturn(mockGraph);
+    when(defaultGraphResolver.resolve)
+      .expectCalledWith(expect.anything(), MainGraph, props)
+      .mockReturnValue(mockGraph);
 
     const graph = uut.resolve(MainGraph, props);
     expect(graph).toEqual(mockGraph);
   });
 
   it('Resolves middlewares in LIFO order', () => {
-    const anotherResolver: Middleware<ObjectGraph> = imock();
-    when(anotherResolver.resolve(anything(), anything(), anything())).thenReturn(mockGraph);
+    const anotherResolver = mock<Middleware<ObjectGraph>>();
+    when(anotherResolver.resolve).mockReturnValue(mockGraph);
 
-    uut.add(instance(anotherResolver));
+    uut.add(anotherResolver);
     const result = uut.resolve(MainGraph);
     expect(result).toEqual(mockGraph);
-    verify(defaultGraphResolver.resolve(anything(), anything())).never();
+    expect(defaultGraphResolver.resolve).not.toHaveBeenCalled();
   });
 });
