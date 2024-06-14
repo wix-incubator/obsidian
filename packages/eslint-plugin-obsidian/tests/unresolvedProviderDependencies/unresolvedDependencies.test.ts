a747e7a1d5e39156197ebf746dbb193df5d79d5f
Guy Carmeli <guy.gc.gc@gmail.com>
2024-06-14 07:45:05 +0300

    Move tests to a dedicated folder

---
+++ packages/eslint-plugin-obsidian/tests/unresolvedProviderDependencies/unresolvedDependencies.test.ts
@@ -0,0 +1,18 @@
+import { RuleTester } from '@typescript-eslint/rule-tester';
+import { validGraphSimple } from '../../rules/unresolvedProviderDependencies/testUtils/validGraphs';
+import { unresolvedProviderDependencies } from '../../rules/unresolvedProviderDependencies';
+import { invalidGraph } from '../../rules/unresolvedProviderDependencies/invalidGraphs';
+
+const ruleTester = new RuleTester();
+
+ruleTester.run('unresolved-provider-dependencies', unresolvedProviderDependencies, {
+  valid: [validGraphSimple],
+  invalid: [
+    {
+      code: invalidGraph,
+      errors: [{
+        messageId: 'unresolved-provider-dependencies',
+      }],
+    },
+  ],
+});
