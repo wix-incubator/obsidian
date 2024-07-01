263ef89b95c67474fddbb4ae1ffa098218d35508
Guy Carmeli <guyca@users.noreply.github.com>
2024-07-01 20:30:57 +0300

    Upgrade to Docusaurus v3 (#153)
    * Upgrade to docusaurus v3
    
    * Update docusaurus dependencies
    
    * Fix broken links

--- packages/documentation/docusaurus.config.js
+++ packages/documentation/docusaurus.config.js
@@ -1,8 +1,9 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const {themes} = require('prism-react-renderer');
+const lightTheme = themes.github;
+const darkTheme = themes.dracula;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -148,8 +149,8 @@ const config = {
         copyright: `Copyright © ${new Date().getFullYear()} Wix.com. Built with Docusaurus.`,
       },
       prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
+        theme: lightTheme,
+        darkTheme,
       },
     }),
 };
