4cbfd8db6f762d7a6966365f04b837d76f9ba198
Guy Carmeli <guy.gc.gc@gmail.com>
2024-10-03 12:18:19 +0300

    Test and document abstract graphs
    Abstract graphs is a handy approach to declare common dependencies that are shared by multiple graphs.

---
+++ packages/react-obsidian/test/acceptance/abstractGraph.test.ts
@@ -0,0 +1,52 @@
+import { Graph, ObjectGraph, Obsidian, Provides } from "../../src";
+
+describe("abstract graph", () => {
+  it('should be able to create a graph', () => {
+    expect(new FooGraph()).toBeInstanceOf(FooGraph);
+  });
+
+  it('should be able to provide a value', () => {
+    expect(Obsidian.obtain(FooGraph).atomicDependency()).toBe('foo');
+  });
+
+  it('should be able to provide a composite value', () => {
+    expect(Obsidian.obtain(FooGraph).compositeDependency()).toBe('foobar');
+  });
+
+  it('should provide dependencies that depend on abstract dependencies', () => {
+    expect(Obsidian.obtain(FooGraph).dependsOnAbstractDependency()).toBe('depends on baz');
+  });
+});
+
+
+abstract class AbstractGraph extends ObjectGraph {
+  @Provides()
+  compositeDependency(atomicDependency: string, bar: string) {
+    return atomicDependency + bar;
+  }
+
+  @Provides()
+  atomicDependency() {
+    return 'foo';
+  }
+
+  @Provides()
+  dependsOnAbstractDependency(baz: string) {
+    return `depends on ${baz}`;
+  }
+
+  abstract baz(): string;
+}
+
+@Graph()
+class FooGraph extends AbstractGraph {
+  @Provides()
+  bar() {
+    return 'bar';
+  }
+
+  @Provides()
+  override baz() {
+    return 'baz';
+  }
+}
