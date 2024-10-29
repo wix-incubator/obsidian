99f16b743ebbc7c74d38e0ad7d33c5fe94c72433
Guy Carmeli <guy.gc.gc@gmail.com>
2024-10-29 19:38:26 +0200

    inject component with registered graphs

--- packages/react-obsidian/src/injectors/components/useInjectionToken.ts
+++ packages/react-obsidian/src/injectors/components/useInjectionToken.ts
@@ -2,9 +2,12 @@ import { useContext, useState } from 'react';
 import { GraphContext } from './graphContext';
 import type { Constructable, ObjectGraph } from '../..';
 import { uniqueId } from '../../utils/uniqueId';
+import { isString } from '../../utils/isString';
 
-export const useInjectionToken = (Graph: Constructable<ObjectGraph>) => {
+export const useInjectionToken = (keyOrGraph: string | Constructable<ObjectGraph>) => {
   const ctx = useContext(GraphContext);
-  const [injectionToken] = useState(() => ctx?.injectionToken ?? uniqueId(Graph.name));
+  const [injectionToken] = useState(() => {
+    return ctx?.injectionToken ?? uniqueId(isString(keyOrGraph)? keyOrGraph : keyOrGraph.name);
+  });
   return injectionToken;
 };
