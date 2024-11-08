ad4a5c17b8fd9e526b6d626b7e7215fcf5a94094
Guy Carmeli <guy.gc.gc@gmail.com>
2024-11-08 06:44:49 +0200

    Move the demo app to the SWC plugin folder

--- packages/swc-plugin-obsidian/demo/vite.config.ts
+++ packages/swc-plugin-obsidian/demo/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vitest/config.js';
 import react from '@vitejs/plugin-react';
 import Inspect from 'vite-plugin-inspect'
-import obsidian from 'swc-plugin-obsidian/dist'
+import obsidian from 'swc-plugin-obsidian'
 
 // https://vitejs.dev/config/
 export default defineConfig({
