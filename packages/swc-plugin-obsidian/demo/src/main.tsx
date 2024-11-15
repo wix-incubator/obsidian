fab257f4d40256781393272b383cc6c6c6b0bd9e
Guy Carmeli <guy.gc.gc@gmail.com>
2024-11-15 14:30:11 +0200

    Clean up
    Remove lint and flatten folder structure

--- packages/swc-plugin-obsidian/demo/src/main.tsx
+++ packages/swc-plugin-obsidian/demo/src/main.tsx
@@ -3,13 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Example } from './Example';
 
-async function prepare() {
-}
-
-prepare().then(() => {
-  ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-      <Example />
-    </React.StrictMode>,
-  );
-});
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Example />
+  </React.StrictMode>,
+);
