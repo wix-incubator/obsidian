1d06a66c3fd50a6edd7840437c4055cdfcb181f2
Guy Carmeli <guy.gc.gc@gmail.com>
2024-11-21 05:42:15 +0200

    Documentation

--- packages/swc-plugin-obsidian/demo/vite.config.ts
+++ packages/swc-plugin-obsidian/demo/vite.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react-swc';
 import Inspect from 'vite-plugin-inspect'
 import swc from 'unplugin-swc'
 import obsidian from '../src';
