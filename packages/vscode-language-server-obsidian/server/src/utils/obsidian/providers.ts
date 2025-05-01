f539cf70cbc0b25568a62a644627700b2125dabb
Guy Carmeli <guy.gc.gc@gmail.com>
2025-05-01 19:28:39 +0300

    f

--- packages/vscode-language-server-obsidian/server/src/utils/obsidian/providers.ts
+++ packages/vscode-language-server-obsidian/server/src/utils/obsidian/providers.ts
@@ -3,19 +3,7 @@ import { Node, ArrowFunction, FunctionTypeNode, SyntaxKind } from "ts-morph";
 export function getHookDeclaration(node: ArrowFunction) {
   const body = node.getBody();
   if (Node.isCallExpression(body)) {
-    const expression = body.getExpression();
-    if (Node.isIdentifier(expression)) {
-      const references = expression.findReferencesAsNodes() || [];
-      for (const reference of references) {
-        const symbol = reference.getSymbol();
-        if (symbol && symbol.getDeclarations().length > 0) {
-          const declaration = symbol.getDeclarations()[0];
-          if (Node.isVariableDeclaration(declaration)) {
-            return declaration;
-          }
-        }
-      }
-    }
+    return getDeclarationFromIdentifier(body.getExpression());
   }
 }
 
@@ -23,16 +11,19 @@ export function getHookDecarationFromTypedProvider(node: FunctionTypeNode) {
   const typeReference = node.getChildrenOfKind(SyntaxKind.TypeReference)[0];
   const arg = typeReference.getTypeArguments()[0];
   if (Node.isTypeQuery(arg)) {
-    const type = arg.getExprName();
-    if (Node.isIdentifier(type)) {
-      const references = type.findReferencesAsNodes() || [];
-      for (const reference of references) {
-        const symbol = reference.getSymbol();
-        if (symbol?.getDeclarations().length === 1) {
-          const declaration = symbol.getDeclarations()[0];
-          if (Node.isVariableDeclaration(declaration)) {
-            return declaration;
-          }
+    return getDeclarationFromIdentifier(arg.getExprName());
+  }
+}
+
+function getDeclarationFromIdentifier(node: Node) {
+  if (Node.isIdentifier(node)) {
+    const references = node.findReferencesAsNodes() || [];
+    for (const reference of references) {
+      const symbol = reference.getSymbol();
+      if (symbol?.getDeclarations().length === 1) {
+        const declaration = symbol.getDeclarations()[0];
+        if (Node.isVariableDeclaration(declaration)) {
+          return declaration;
         }
       }
     }
