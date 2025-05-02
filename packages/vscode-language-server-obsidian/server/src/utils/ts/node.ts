826963a68d5c8843bb93d91333a62f4000b33050
Guy Carmeli <guy.gc.gc@gmail.com>
2025-05-02 14:08:26 +0300

    f

--- packages/vscode-language-server-obsidian/server/src/utils/ts/node.ts
+++ packages/vscode-language-server-obsidian/server/src/utils/ts/node.ts
@@ -1,14 +1,11 @@
 import { Node } from "ts-morph";
 import { hasParentWithDecorator } from "./decorators";
 
-export function isInjected(node: Node | undefined) {
-  if (node && Node.isIdentifier(node)) {
-    for (const referencedSymbol of node.findReferences()) {
-      for (const reference of referencedSymbol.getReferences()) {
-        if (hasParentWithDecorator(reference.getNode(), ['Provides', 'provides'])) {
-          return true;
-        }
-      }
+export function isInjected(node: Node | undefined): boolean {
+  if (!node || !Node.isIdentifier(node)) return false;
+  for (const reference of node.findReferencesAsNodes()) {
+    if (hasParentWithDecorator(reference, ['Provides', 'provides'])) {
+      return true;
     }
   }
   return false;
