0de12665e0fe97c82b7785bce2c4b6ab57f28430
Guy Carmeli <guy.gc.gc@gmail.com>
2025-10-11 19:48:30 +0300

    Consume ts-morph-extensions from dist + detect nested classes

--- packages/eslint-plugin-obsidian/src/ts/adapters/classAdapter.ts
+++ packages/eslint-plugin-obsidian/src/ts/adapters/classAdapter.ts
@@ -1,12 +1,23 @@
 import { ProjectRegistry , Graph } from 'ts-morph-extensions';
 import type { TSESTree } from '@typescript-eslint/types';
+import { SourceFile, SyntaxKind } from 'ts-morph';
 
 export class ClassAdapter {
   constructor(private readonly projectRegistry: ProjectRegistry) { }
 
   public classToGraph(node: TSESTree.ClassDeclaration, filePath: string): Graph | undefined {
     if (!node.id?.name) return;
-    const clazz = this.projectRegistry.getSourceFile(filePath)?.getClass(node.id.name);
-    return clazz ? new Graph(clazz) : undefined;
+    const sourceFile = this.projectRegistry.getSourceFile(filePath);
+    let clazz = this.topLevelClass(sourceFile, node.id.name) ?? this.nestedClass(sourceFile, node.id.name);
+    return clazz && new Graph(clazz);
+  }
+
+  private topLevelClass(sourceFile: SourceFile | undefined, className: string) {
+    return sourceFile?.getClass(className);
+  }
+
+  private nestedClass(sourceFile: SourceFile | undefined, className: string) {
+    const allClasses = sourceFile?.getDescendantsOfKind(SyntaxKind.ClassDeclaration);
+    return allClasses?.find(c => c.getName() === className);
   }
 }
