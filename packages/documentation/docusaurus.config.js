782bd50cb93c0ae2c83cf22bc2284d76542650ab
Guy Carmeli <guyca@users.noreply.github.com>
2025-12-31 09:21:00 +0200

    Fix no search results (#264)

--- packages/documentation/docusaurus.config.js
+++ packages/documentation/docusaurus.config.js
@@ -156,10 +156,10 @@ const config = {
         copyright: `Copyright © ${new Date().getFullYear()} Wix.com. Built with Docusaurus.`,
       },
       algolia: {
-        container: '#docsearch',
         appId: 'XC2HBV2N8Q',
         apiKey: 'b52141250392f5cc0957e046882dc267',
         indexName: 'Documentation Website',
+        contextualSearch: false,
       },
       prism: {
         theme: lightTheme,
