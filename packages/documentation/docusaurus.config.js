5697a6743b1a3169d90bb905ee9777e708c8a62b
Guy Carmeli <guy.gc.gc@gmail.com>
2026-01-01 06:51:19 +0200

    Fix edit docs button leading to wrong repo

--- packages/documentation/docusaurus.config.js
+++ packages/documentation/docusaurus.config.js
@@ -44,15 +44,11 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/wix-incubator/obsidian/tree/master/packages/documentation/',
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/wix-incubator/obsidian/tree/master/packages/documentation/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
