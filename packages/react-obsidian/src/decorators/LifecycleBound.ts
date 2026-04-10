b145dc010ffc76667d6d09ffc2a1e9b8b6ca73c9
Guy Carmeli <guyc@wix.com>
2026-04-10 13:01:33 +0300

    Align default inactiveBehavior with docs

--- packages/react-obsidian/src/decorators/LifecycleBound.ts
+++ packages/react-obsidian/src/decorators/LifecycleBound.ts
@@ -9,7 +9,7 @@ export function lifecycleBound(options?: Options) {
   return (constructor: any) => {
     Reflect.defineMetadata('isLifecycleBound', true, constructor);
     Reflect.defineMetadata('lifecycleScope', options?.scope ?? 'feature', constructor);
-    Reflect.defineMetadata('inactiveBehavior', options?.inactiveBehavior ?? 'unmount', constructor);
+    Reflect.defineMetadata('inactiveBehavior', options?.inactiveBehavior ?? 'retain', constructor);
     return constructor;
   };
 }
